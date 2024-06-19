8afc1aa5196b4e5678698f9d7a60f8f151ce6f76
EL-OUARDY <ouadie.elouardy@gmail.com>
2024-06-19 12:40:39 +0100

    Remove ui componenrts

--- src/pages/Shadcn.tsx
+++ src/pages/Shadcn.tsx
@@ -1,13 +1,10 @@
-import { CardWithForm } from "@/components/ui/CardWithForm";
 import { ThemeProvider } from "@/components/theme-provider";
 
 function Shadcn() {
   return (
     <>
-      <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-        <div className="flex justify-center items-center h-screen bg-secondary">
-          <CardWithForm></CardWithForm>
-        </div>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <div></div>
       </ThemeProvider>
     </>
   );
